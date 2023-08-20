83c0a60643d71a4e039a7386322e9867fd996fed
wangxiang <276644985haidnor@gmail.com>
2023-08-20 15:04:08 +0800

    update NettyRemotingClient.java

--- src/main/java/haidnor/remoting/ChannelEventListener.java
+++ src/main/java/haidnor/remoting/ChannelEventListener.java
@@ -3,11 +3,15 @@ package haidnor.remoting;
 import io.netty.channel.Channel;
 
 public interface ChannelEventListener {
-    void onChannelConnect(final String remoteAddr, final Channel channel);
+    default void onChannelConnect(final String remoteAddr, final Channel channel) {
+    }
 
-    void onChannelClose(final String remoteAddr, final Channel channel);
+    default void onChannelClose(final String remoteAddr, final Channel channel) {
+    }
 
-    void onChannelException(final String remoteAddr, final Channel channel);
+    default void onChannelException(final String remoteAddr, final Channel channel) {
+    }
 
-    void onChannelIdle(final String remoteAddr, final Channel channel);
+    default void onChannelIdle(final String remoteAddr, final Channel channel) {
+    }
 }
