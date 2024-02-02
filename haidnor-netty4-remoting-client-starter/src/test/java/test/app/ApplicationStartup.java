8301e0d2d67cee9645829f8d78ca4ded5848980b
Franz Haidnor <haidnor@outlook.com>
2024-02-02 18:27:31 +0800

    update

--- haidnor-netty4-remoting-client-starter/src/test/java/test/app/ApplicationStartup.java
+++ haidnor-netty4-remoting-client-starter/src/test/java/test/app/ApplicationStartup.java
@@ -2,12 +2,10 @@ package test.app;
 
 import haidnor.remoting.core.NettyRemotingClient;
 import haidnor.remoting.protocol.RemotingCommand;
-import haidnor.remoting.util.Jackson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import test.model.User;
 
 @Component
 public class ApplicationStartup implements ApplicationRunner {
@@ -17,9 +15,17 @@ public class ApplicationStartup implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        RemotingCommand request = RemotingCommand.creatJsonProtocolRequest("/user/login", new User(1L, "888888ABC123456"));
+//        RemotingCommand request = RemotingCommand.creatJsonProtocolRequest("/user/login", new User(1L, "888888ABC123456"));
+//        RemotingCommand response = client.invokeSync("127.0.0.1:8080", request);
+//        System.out.println(Jackson.toBean(response.getBody(),String.class));
+
+        // 创建请求命令
+        RemotingCommand request = RemotingCommand.creatRequest("/test/echo", "Hello World".getBytes());
+        // 发送请求
         RemotingCommand response = client.invokeSync("127.0.0.1:8080", request);
-        System.out.println(Jackson.toBean(response.getBody(),String.class));
+        // 打印服务端响应消息
+        System.out.println(new String(response.getBody()));
+
 
 //        client.invokeOneway("127.0.0.1:8080", request);
 
--- haidnor-netty4-remoting-server-starter/src/test/java/test/processor/LoginProcessor.java
+++ haidnor-netty4-remoting-server-starter/src/test/java/test/processor/LoginProcessor.java
@@ -2,6 +2,8 @@ package test.processor;
 
 import haidnor.remoting.client.spring.common.annotation.NettyRemotingRequestProcessor;
 import haidnor.remoting.client.spring.common.processor.JsonProtocolNettyRequestProcessorAdapter;
+import haidnor.remoting.util.ChannelHandlerContextHolder;
+import io.netty.channel.ChannelHandlerContext;
 import org.springframework.stereotype.Component;
 import test.model.User;
 
@@ -11,6 +13,7 @@ public class LoginProcessor extends JsonProtocolNettyRequestProcessorAdapter<Use
 
     @Override
     public Object processRequest(User user) {
+        ChannelHandlerContext context = ChannelHandlerContextHolder.get();
         System.out.println(user);
         return "OK";
     }
